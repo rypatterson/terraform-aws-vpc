38318c10871207a643ad13609237105f4d3600d0
Patterson <ryptter@147dda0c265b.ant.amazon.com>
2021-03-16 14:35:08 -0500

    vpc-dev

--- examples/complete-vpc/main.tf
+++ examples/complete-vpc/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-2"
 }
 
 data "aws_security_group" "default" {
@@ -10,17 +10,17 @@ data "aws_security_group" "default" {
 module "vpc" {
   source = "../../"
 
-  name = "complete-example"
+  name = "dev"
 
   cidr = "20.10.0.0/16" # 10.0.0.0/8 is reserved for EC2-Classic
 
-  azs                 = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs                 = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
   public_subnets      = ["20.10.11.0/24", "20.10.12.0/24", "20.10.13.0/24"]
   database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
-  elasticache_subnets = ["20.10.31.0/24", "20.10.32.0/24", "20.10.33.0/24"]
-  redshift_subnets    = ["20.10.41.0/24", "20.10.42.0/24", "20.10.43.0/24"]
-  intra_subnets       = ["20.10.51.0/24", "20.10.52.0/24", "20.10.53.0/24"]
+#  elasticache_subnets = ["20.10.31.0/24", "20.10.32.0/24", "20.10.33.0/24"]
+#  redshift_subnets    = ["20.10.41.0/24", "20.10.42.0/24", "20.10.43.0/24"]
+#  intra_subnets       = ["20.10.51.0/24", "20.10.52.0/24", "20.10.53.0/24"]
 
   create_database_subnet_group = false
 
@@ -30,8 +30,8 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  enable_classiclink             = true
-  enable_classiclink_dns_support = true
+  enable_classiclink             = false
+  enable_classiclink_dns_support = false
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -53,41 +53,42 @@ module "vpc" {
   dhcp_options_domain_name         = "service.consul"
   dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
 
+/*
   # VPC endpoint for S3
   # Note - S3 Interface type support is only available on AWS provider 3.10 and later
-  enable_s3_endpoint              = true
+  enable_s3_endpoint              = false
   s3_endpoint_type                = "Interface"
   s3_endpoint_private_dns_enabled = false
   s3_endpoint_security_group_ids  = [data.aws_security_group.default.id]
 
   # VPC endpoint for DynamoDB
-  enable_dynamodb_endpoint = true
+  enable_dynamodb_endpoint = false
   dynamodb_endpoint_policy = data.aws_iam_policy_document.dynamodb_endpoint_policy.json
 
   # VPC endpoint for SSM
-  enable_ssm_endpoint              = true
-  ssm_endpoint_private_dns_enabled = true
+  enable_ssm_endpoint              = false
+  ssm_endpoint_private_dns_enabled = false
   ssm_endpoint_security_group_ids  = [data.aws_security_group.default.id]
 
   # VPC endpoint for Lambda
-  enable_lambda_endpoint              = true
-  lambda_endpoint_private_dns_enabled = true
+  enable_lambda_endpoint              = false
+  lambda_endpoint_private_dns_enabled = false
   lambda_endpoint_security_group_ids  = [data.aws_security_group.default.id]
 
   # VPC endpoint for SSMMESSAGES
-  enable_ssmmessages_endpoint              = true
-  ssmmessages_endpoint_private_dns_enabled = true
+  enable_ssmmessages_endpoint              = false
+  ssmmessages_endpoint_private_dns_enabled = false
   ssmmessages_endpoint_security_group_ids  = [data.aws_security_group.default.id]
 
   # VPC Endpoint for EC2
-  enable_ec2_endpoint              = true
+  enable_ec2_endpoint              = false
   ec2_endpoint_policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
-  ec2_endpoint_private_dns_enabled = true
+  ec2_endpoint_private_dns_enabled = false
   ec2_endpoint_security_group_ids  = [data.aws_security_group.default.id]
 
   # VPC Endpoint for EC2MESSAGES
-  enable_ec2messages_endpoint              = true
-  ec2messages_endpoint_private_dns_enabled = true
+  enable_ec2messages_endpoint              = false
+  ec2messages_endpoint_private_dns_enabled = false
   ec2messages_endpoint_security_group_ids  = [data.aws_security_group.default.id]
 
   # VPC Endpoint for ECR API
@@ -131,67 +132,23 @@ module "vpc" {
   manage_default_security_group  = true
   default_security_group_ingress = []
   default_security_group_egress  = []
+*/
+
 
   # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
-  enable_flow_log                      = true
-  create_flow_log_cloudwatch_log_group = true
-  create_flow_log_cloudwatch_iam_role  = true
+  enable_flow_log                      = false
+  create_flow_log_cloudwatch_log_group = false
+  create_flow_log_cloudwatch_iam_role  = false
   flow_log_max_aggregation_interval    = 60
 
   tags = {
     Owner       = "user"
-    Environment = "staging"
-    Name        = "complete"
+    Environment = "dev"
+    Name        = "dev"
   }
 
   vpc_endpoint_tags = {
     Project  = "Secret"
-    Endpoint = "true"
-  }
-}
-
-# Data source used to avoid race condition
-data "aws_vpc_endpoint" "dynamodb" {
-  vpc_id       = module.vpc.vpc_id
-  service_name = "com.amazonaws.eu-west-1.dynamodb"
-}
-
-data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
-  statement {
-    effect    = "Deny"
-    actions   = ["dynamodb:*"]
-    resources = ["*"]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    condition {
-      test     = "StringNotEquals"
-      variable = "aws:sourceVpce"
-
-      values = [data.aws_vpc_endpoint.dynamodb.id]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "generic_endpoint_policy" {
-  statement {
-    effect    = "Deny"
-    actions   = ["*"]
-    resources = ["*"]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    condition {
-      test     = "StringNotEquals"
-      variable = "aws:sourceVpce"
-
-      values = [data.aws_vpc_endpoint.dynamodb.id]
-    }
+    Endpoint = "false"
   }
 }
